035fc8cf97b8a652f64f2991196b035fd825fad9
Ik Jum Moon <86935775+junsuboy@users.noreply.github.com>
2022-04-06 18:58:38 +0900

    이메일 로그인, 구글 로그인 구현

--- SpotifyLoginSampleApp/SpotifyLoginSampleApp/LoginViewController.swift
+++ SpotifyLoginSampleApp/SpotifyLoginSampleApp/LoginViewController.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
+import GoogleSignIn
+import FirebaseAuth
+import Firebase
 
 class LoginViewController: UIViewController {
     
     @IBOutlet weak var emailLoginButton: UIButton!
-    @IBOutlet weak var googleLogiinButton: UIButton!
+    @IBOutlet weak var googleLogiinButton: GIDSignInButton!
     @IBOutlet weak var appleLoginButton: UIButton!
     
     override func viewDidLoad() {
@@ -28,10 +31,36 @@ class LoginViewController: UIViewController {
         
         // Navigation Bar 숨기기
         navigationController?.navigationBar.isHidden = true
+        
+    }
+    
+    private func showMainViewController() {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let mainViewController = storyboard.instantiateViewController(withIdentifier: "MainViewController")
+        mainViewController.modalPresentationStyle = .fullScreen
+        UIApplication.shared.windows.first?.rootViewController?.show(mainViewController, sender: nil)
     }
     
     @IBAction func googleLoginButtonTapped(_ sender: UIButton) {
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.signIn(with: config, presenting: self) { [unowned self] user, error in
+            if let error = error {
+                print("ERROR", error.localizedDescription)
+                return
+            }
+            
+            guard let authentication = user?.authentication,
+                  let idToken = authentication.idToken else { return }
+            
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: authentication.accessToken)
+            
+            Auth.auth().signIn(with: credential) { _, _ in
+                self.showMainViewController()
+            }
+        }
     }
+    
     @IBAction func appleLoginButtonTapped(_ sender: UIButton) {
     }
     
--- SpotifyLoginSampleApp/SpotifyLoginSampleApp/MainViewController.swift
+++ SpotifyLoginSampleApp/SpotifyLoginSampleApp/MainViewController.swift
@@ -6,9 +6,11 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class MainViewController: UIViewController {
     @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var resetPasswordButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,8 +21,29 @@ class MainViewController: UIViewController {
         super.viewWillAppear(animated)
         
         navigationController?.navigationBar.isHidden = true
+        
+        let email = Auth.auth().currentUser?.email ?? "고객"
+        
+        welcomeLabel.text = """
+        환영합니다.
+        \(email)님
+        """
+        
+        let isEmailSignIn = Auth.auth().currentUser?.providerData[0].providerID == "password"
+        resetPasswordButton.isHidden = !isEmailSignIn
     }
     @IBAction func logoutButtonTapped(_ sender: Any) {
-        self.navigationController?.popToRootViewController(animated: true)
+        let firebaseAuth = Auth.auth()
+        
+        do {
+            try firebaseAuth.signOut()
+            self.navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print("ERROR: signout \(signOutError.localizedDescription)")
+        }
+    }
+    @IBAction func resetPasswordButtonTapped(_ sender: UIButton) {
+        let email = Auth.auth().currentUser?.email ?? ""
+        Auth.auth().sendPasswordReset(withEmail: email, completion: nil)
     }
 }
