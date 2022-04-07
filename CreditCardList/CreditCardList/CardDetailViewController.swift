7f4f6284da38d5b958a12d39576c3bded8b9eda2
Ik Jum Moon <86935775+junsuboy@users.noreply.github.com>
2022-04-07 17:49:58 +0900

    import 'lottie-ios'
    gif 오픈소스 라이브러리

---
+++ CreditCardList/CreditCardList/CardDetailViewController.swift
@@ -0,0 +1,12 @@
+//
+//  CardDetailViewController.swift
+//  CreditCardList
+//
+//  Created by Junsu Jang on 2022/04/07.
+//
+
+import UIKit
+
+class CardDetailViewController: UIViewController {
+    
+}
