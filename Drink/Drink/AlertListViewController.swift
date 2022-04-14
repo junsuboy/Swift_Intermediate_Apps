0bea56033ec041362b39e3b3e4672a691c709b5d
Ik Jum Moon <86935775+junsuboy@users.noreply.github.com>
2022-04-15 01:16:24 +0900

    물마시기앱 UI 만들다 맒

--- Drink/Drink/AlertListViewController.swift
+++ Drink/Drink/AlertListViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class AlertListViewController: UITableViewController {
+    var alertList: [Alert] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -16,5 +18,51 @@ class AlertListViewController: UITableViewController {
     }
     
     @IBAction func addAlertButtonAction(_ sender: Any) {
+    }
+}
+
+// UITableView Datasource, Delegate
+extension AlertListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return alertList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "🚰 물마실 시간"
+        default:
+            return nil
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "AlertListCell", for: indexPath)
+                as? AlertListCell else { return UITableViewCell() }
+        
+        cell.alertSwitch.isOn = alertList[indexPath.row].isOn
+        cell.timeLabel.text = alertList[indexPath.row].time
+        cell.meridiemLabel.text = alertList[indexPath.row].meridiem
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        switch editingStyle {
+        case .delete:
+            //노티피케이션 삭제 구현
+            return
+        default:
+            break
+            }
+        }
     }
 }
---
+++ Drink/Drink/addAlertViewController.swift
@@ -0,0 +1,12 @@
+//
+//  addAlertViewController.swift
+//  Drink
+//
+//  Created by Junsu Jang on 2022/04/14.
+//
+
+import UIKit
+
+class AddAlertViewController: UIViewController {
+    
+}
