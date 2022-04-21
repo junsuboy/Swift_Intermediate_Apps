a3598eb9a6aa134db4809459c55723d68ad06fbd
Ik Jum Moon <86935775+junsuboy@users.noreply.github.com>
2022-04-22 02:51:53 +0900

    import Snapkit

---
+++ NetfilxStyleSampleApp/NetfilxStyleSampleApp/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  NetfilxStyleSampleApp
+//
+//  Created by Junsu Jang on 2022/04/22.
+//
+
+import UIKit
+
+class HomeViewController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 네비게이션 설정
+    }
+}
