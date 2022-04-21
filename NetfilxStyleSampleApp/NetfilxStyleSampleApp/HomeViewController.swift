c73c1a5363d9fa7eac6f28ee97fd259625d12652
Ik Jum Moon <86935775+junsuboy@users.noreply.github.com>
2022-04-22 03:13:17 +0900

    영화 이미지 추가, 영화 배열 추가

--- NetfilxStyleSampleApp/NetfilxStyleSampleApp/HomeViewController.swift
+++ NetfilxStyleSampleApp/NetfilxStyleSampleApp/HomeViewController.swift
@@ -12,5 +12,12 @@ class HomeViewController: UICollectionViewController {
         super.viewDidLoad()
         
         // 네비게이션 설정
+        navigationController?.navigationBar.backgroundColor = .clear
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.hidesBarsOnSwipe = true
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "netflix_icon.png"), style: .plain, target: nil, action: nil)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "person.crop.circle"), style: .plain, target: nil, action: nil)
     }
 }
