324414d0124cfc4a2dd16dc40f8e80797afb5811
Ik Jum Moon <86935775+junsuboy@users.noreply.github.com>
2022-04-27 23:18:04 +0900

    기본 설정 완료

---
+++ Brewary/Brewary/BeerListViewController.swift
@@ -0,0 +1,12 @@
+//
+//  BeerListViewController.swift
+//  Brewary
+//
+//  Created by Junsu Jang on 2022/04/27.
+//
+
+import UIKit
+
+class BeerListViewController: UITableViewController {
+    
+}
